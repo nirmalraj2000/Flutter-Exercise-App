d08530dc7b46bf6025aff8e413b0411659c5d7e2
Nirmal Raj <nirmalkform1815@gmail.com>
2020-05-26 15:43:58 +0530

    App Logo added

--- lib/Screens/CreaterSection.dart
+++ lib/Screens/CreaterSection.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class CreaterPage extends StatefulWidget {
   @override
@@ -6,6 +7,8 @@ class CreaterPage extends StatefulWidget {
 }
 
 class _CreaterPageState extends State<CreaterPage> {
+  final String url = "https://youtu.be/VFrKjhcTAzE";
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -24,8 +27,8 @@ class _CreaterPageState extends State<CreaterPage> {
                 Padding(
                   padding: EdgeInsets.only(top: 30, bottom: 10),
                   child: Container(
-                    height: 200,
-                    width: 200,
+                    height: 180,
+                    width: 180,
                     child: CircleAvatar(
                       backgroundImage: AssetImage("Images/createrImage.jpg"),
                     ),
@@ -41,16 +44,33 @@ class _CreaterPageState extends State<CreaterPage> {
                     "This app was created to people who wish to do their daily routine of workout without any deviation." +
                         " Whenever the user started their workout the application will play the music. The music is stopped during the break time. " +
                         "I took nearly 40 hours to complete this app." +
-                        "I created this app with my own curiosity. " +
+                        "I created this app with my own curiosity and also as a challenge (challenge video link below). " +
                         "If any body want to purchase this app, I would surely customize as per the customer's preferences.",
-                    style: TextStyle(fontSize: 18),
+                    style: TextStyle(fontSize: 17),
                     textAlign: TextAlign.justify,
                   ),
                 ),
-                Padding(padding: EdgeInsets.only(top: 30),
-                
-                child: Text(r"$ 49",style: TextStyle(fontSize: 40,color: Colors.blue),)
-                )
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: GestureDetector(
+                    onTap: () async {
+                      if (await canLaunch(url)) {
+                        launch(url);
+                      }
+                    },
+                    child: Text(
+                      url,
+                      style: TextStyle(fontSize: 18,color:Colors.blue),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                Padding(
+                    padding: EdgeInsets.only(top: 20),
+                    child: Text(
+                      r"$ 49",
+                      style: TextStyle(fontSize: 40, color: Colors.blue),
+                    ))
               ],
             ),
           )),
